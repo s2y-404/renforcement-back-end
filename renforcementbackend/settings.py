b472c0b2307023e608f34db9c9806a559f1dd2a8
s2y-404 <syd95@hotmail.fr>
2024-10-18 21:05:48 +0200

    Throttling

--- renforcementbackend/settings.py
+++ renforcementbackend/settings.py
@@ -83,6 +83,14 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '10/minute',
+    }
 }
 
 
