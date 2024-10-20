c48538b9b557d83864af0fcb6815e7c4aa6edb1d
Sydney Marcelin <75375209+s2y-404@users.noreply.github.com>
2024-10-20 21:55:30 +0200

    Update settings.py

--- renforcementbackend/settings.py
+++ renforcementbackend/settings.py
@@ -165,11 +165,13 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+import os
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sabineaurelie1@gmail.com'
-EMAIL_HOST_PASSWORD = "Ceci est 1 compte fake."
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
